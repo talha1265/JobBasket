c615c28bc834c10c153edf02c4769bf4f94a5a8d
talha1265 <132248831+talha1265@users.noreply.github.com>
2025-02-01 10:56:08 -0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,18 @@
 const jobs = [
+    {
+        title: "Software/ QA intern (Fresher)",
+        company: "Bluelupin Technologies Pvt. Ltd.",
+        location: "Noida, Uttar Pradesh, India",
+        date: "2025-02-01",
+        url: "https://www.linkedin.com/jobs/view/4050501272"
+      },
+      {
+        title: "Web Developer Intern",
+        company: "Business Web Solutions (USA)",
+        location: "Bengaluru, Karnataka, India",
+        date: "2025-01-31",
+        url: "https://www.linkedin.com/jobs/view/4139307203"
+      },
     { title: "Software Engineer", company: "Google", location: "California", date: "2025-01-30", url: "https://careers.google.com" },
     { title: "Data Analyst", company: "Amazon", location: "Seattle", date: "2025-01-28", url: "https://amazon.jobs" },
     { title: "Product Manager", company: "Microsoft", location: "New York", date: "2025-01-27", url: "https://careers.microsoft.com" },
